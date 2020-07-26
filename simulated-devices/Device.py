893cc555c0bcbb31a6183a7b97f204e32c4e32ca
unknown <jarod.cunningham@students.mq.edu.au>
2020-07-26 21:58:28 +1000

    added comments to Device.py

--- simulated-devices/Device.py
+++ simulated-devices/Device.py
@@ -8,12 +8,18 @@ SPEED_MIN: int = 10
 SPEED_MAX: int = 300
 
 def _generate_speed() -> float:
+    """ Returns a random number between min and max speed to use as the new wind speed """
     return random.uniform(SPEED_MIN,SPEED_MAX)
 
 def _generate_direction(old_direction: float, speed: float) -> float:
+    """ Uses the previous wind direction and current wind speed to calculate and return a new wind direction """
+    #randomly chooses whether to go clockwise or counter-clockwise and adds half the speed to the previous direction
+    #(this is making the assumption that more energetic winds change more violently)
     direction: float = old_direction + random.choice([1,-1])*speed/2
+    #if 360 or over, then fix it so it remains within a range of 0 to 360
     if direction >= 360:
         direction -= 360
+    #if under 0, then fix it so it remains within the range of 0 to 360
     if direction < 0:
         direction = 360 + direction
     return direction
@@ -21,18 +27,23 @@ def _generate_direction(old_direction: float, speed: float) -> float:
 def run():
     try:
         speed: float = 0.0
+        #initialises direction to a random number since _generate_direction needs something to start with
         direction: float = random.uniform(0,360)
+        #establish connection
         client: IoTHubDeviceClient = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
         print("Sending messages, press Ctrl-c to exit")
         while True:
+            #update wind speed and direction
             speed = _generate_speed()
             direction = _generate_direction(direction,speed)
+            #create and send message
             message: str = '{"wind_speed":'+str(speed)+',"wind_direction":'+str(direction)+'}'
             print("sending: "+message)
-            client.send_message(message)
+            client.send_message(message) #message is actually sent here
             print("sent")
             time.sleep(1)
     except KeyboardInterrupt:
+        #when Ctrl-c is pressed allow the function to finish so the program can stop running
         print("Stopped")
 
 if __name__ == "__main__":
