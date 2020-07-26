76fcfa59d9c43c9eb1ee6c179e35120d0c99527d
unknown <jarod.cunningham@students.mq.edu.au>
2020-07-26 16:22:46 +1000

    device sends randomly generated meaningful data in the form of wind speed and direction

--- simulated-devices/Device.py
+++ simulated-devices/Device.py
@@ -1,15 +1,34 @@
 import time
+import random
 from azure.iot.device import IoTHubDeviceClient, Message
 
 CONNECTION_STRING: str = "HostName=msa-assessment2-project-hub.azure-devices.net;DeviceId=PythonDevice;SharedAccessKey=OJc3CyXMQZE65igOdnLBbibfrQzj0mhfgKv7crUW3Lw="
 
+SPEED_MIN: int = 10
+SPEED_MAX: int = 300
+
+def _generate_speed() -> float:
+    return random.uniform(SPEED_MIN,SPEED_MAX)
+
+def _generate_direction(old_direction: float, speed: float) -> float:
+    direction: float = old_direction + random.choice([1,-1])*speed/2
+    if direction >= 360:
+        direction -= 360
+    if direction < 0:
+        direction = 360 + direction
+    return direction
+
 def run():
     try:
+        speed: float = 0.0
+        direction: float = random.uniform(0,360)
         client: IoTHubDeviceClient = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
         print("Sending messages, press Ctrl-c to exit")
         while True:
-            message: str = '{"test":1.0}'
-            print("sending...")
+            speed = _generate_speed()
+            direction = _generate_direction(direction,speed)
+            message: str = '{"wind_speed":'+str(speed)+',"wind_direction":'+str(direction)+'}'
+            print("sending: "+message)
             client.send_message(message)
             print("sent")
             time.sleep(1)
